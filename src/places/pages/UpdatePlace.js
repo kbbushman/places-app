cab52c651fd9e56826e442cf81e5aa3eaeb267db
kbbushman <k.bushman@yahoo.com>
2019-11-29 16:56:20 -0800

    Adds update place form

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+
+const TEMP_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY, 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY, 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u2',
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const place = TEMP_PLACES.find(p => p.id === placeId);
+
+  if (!place) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE]}
+        errorText="Please enter a valid title."
+        onInput={() => {}}
+        value={place.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        type="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 characters)."
+        onInput={() => {}}
+        value={place.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>UPDATE PLACE</Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
