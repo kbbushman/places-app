792ab868ccb9985ab347b012501e59ac5fcceb71
kbbushman <k.bushman@yahoo.com>
2019-11-30 15:48:19 -0800

    Adds auth context to render links based on auth state

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-content';
 
 import './NavLinks.css';
 
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
         <NavLink to="/" exact>ALL USERS</NavLink>
       </li>
+      {auth.isLoggedIn && (
       <li>
         <NavLink to="/u1/places">MY PLACES</NavLink>
       </li>
+      )}
+      {auth.isLoggedIn && (
       <li>
         <NavLink to="/places/new">ADD PLACE</NavLink>
       </li>
+      )}
+      {!auth.isLoggedIn && (
       <li>
-        <NavLink to="/login">LOGIN</NavLink>
+        <NavLink to="/auth">LOGIN</NavLink>
       </li>
+      )}
     </ul>
   );
 };
