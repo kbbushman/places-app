5b41303ee58f44e57dfbababaf42d7ac596deaa1
kbbushman <k.bushman@yahoo.com>
2019-12-27 20:20:09 -0800

    Adds login

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -57,12 +57,35 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
     // console.log('Form State --->', formState.inputs);
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/v1/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
 
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        // console.log(err);
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5000/api/v1/users/signup', {
           method: 'POST',
           headers: {
