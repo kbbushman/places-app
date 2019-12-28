cf6e608c3a01621ea3a5ee08b7919b837d8e508f
kbbushman <k.bushman@yahoo.com>
2019-12-27 21:28:58 -0800

    Adds custom API hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,47 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const activeHttpRequest = useRef([]);
+
+  const sendRequest = useCallback(async (url, method='GET', body=null, headers={}) => {
+    setIsLoading(true);
+    const httpAbortCtrl = new AbortController();
+    activeHttpRequest.current.push(httpAbortCtrl);
+    try {
+      const response = await fetch(url, {
+        method,
+        headers,
+        body,
+        signal: httpAbortCtrl.signal,
+      });
+  
+      const responseData = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(true);
+  }, []);
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  // If useEffect callback returns a function, it becomes a "componentWillUnmount" cleanup function
+  useEffect(() => {
+    return () => {
+      // Cancel can running API calls when component unmounts
+      activeHttpRequest.current.forEach(abortCtrl => abortCtrl.abort())
+    };
+  }, []);
+
+  return {isLoading, error, sendRequest, clearError}
+};
