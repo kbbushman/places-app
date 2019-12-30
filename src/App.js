391e2042f785addff1887fce6391f3055b207c93
kbbushman <k.bushman@yahoo.com>
2019-12-30 13:22:56 -0800

    Adds user id and token to local storage at login

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   
   const login = useCallback((uid, token) => {
     setToken(token);
+    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token}));
     setUserId(uid);
   }, []);
 
