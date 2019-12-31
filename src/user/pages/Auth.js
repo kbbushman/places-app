6e52d20cf9cbb987a1b6f89cd3f295c096ace6bb
kbbushman <k.bushman@yahoo.com>
2019-12-30 20:55:50 -0800

    Fixes signup endpoint

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -89,7 +89,7 @@ const Auth = () => {
         formData.append('password', formState.inputs.password.value);
         formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
-          `${process.env.REACT_APP_API_URL}/users`,
+          `${process.env.REACT_APP_API_URL}/users/signup`,
           'POST',
           formData,
         );
